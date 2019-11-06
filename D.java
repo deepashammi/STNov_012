426df08d5b031fa2084e923cb69756116b300e02
DEEPA SHAMMI <54535695+deepashammi@users.noreply.github.com>
2019-11-06 08:32:53 +0530

    Add files via upload

---
+++ D.java
@@ -0,0 +1,17 @@
+package a.b;
+
+public class D {
+    public void x() {
+        int abc = 10;
+        int cde = abc * 5;
+        abc = cde + 7;
+        abc=abc+1;
+    }
+{
+    public void z() {
+        int abc = 108;
+        int cde = abc * 5;
+        abc = cde + 7;
+        abc=abc+12;
+
+   }
